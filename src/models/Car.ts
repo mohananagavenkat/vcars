b1cc357f757964a16a8b7114923d80737b33c1a0
mohananagavenkat <mohananagavenkat.s@thoughtworks.com>
2022-02-28 16:18:13 +0530

    cars model and cars data

---
+++ src/models/Car.ts
@@ -0,0 +1,12 @@
+export interface Car {
+  name: string;
+  miles_per_gallon: number;
+  cylinders: number;
+  displacement: number;
+  horsepower: number;
+  weight_in_lbs: number;
+  acceleration: number;
+  year: string;
+  origin: string;
+  image: string;
+}
